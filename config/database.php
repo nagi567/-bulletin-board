a2723d3573602837fea8b3d970c7294f43a34c53
あなたのGitHubユーザー名 <ec2-user@ip-172-31-33-141.ap-northeast-1.compute.internal>
2026-03-03 22:19:53 +0000

    add Procfile

--- config/database.php
+++ config/database.php
@@ -53,10 +53,15 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'connect_timeout' => 60,
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+               PDO::ATTR_TIMEOUT => 60,
+               PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ]) : [],
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
